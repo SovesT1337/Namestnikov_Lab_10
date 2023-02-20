46805e51fd56e3406e78b2f7071fbca2d1ae66de
SovesT1337 <71689243+SovesT1337@users.noreply.github.com>
2023-02-21 01:39:37 +0300

    авы

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 class Factorial:
-    def __init__(self):
-        self.af = 0
+    def __init__(self):  # инициализируем класс
+        self.af = 0  # задаем начальное значение суммы
 
-    def __iter__(self):
-        self.current = 1
-        self.factorial = 1
-        return self
+    def __iter__(self):  # тут мы создаем итератор
+        self.current, self.factorial = 1, 1  # задаем начальные значения текущего номера элемента
+        # и значение его факториала соответственно
+        return self  # возвращаем итератор
 
-    def __next__(self):
-        self.factorial = self.factorial * self.current
-        self.af = self.factorial - self.af
-        self.current += 1
-        return self.af
+    def __next__(self):  # метод класса осуществляющий вычисление каждого последующего значения суммы
+        self.factorial = self.factorial * self.current  # вычисляем значение факториала
+        self.af = self.factorial - self.af  # вычисляем значение суммы
+        self.current += 1  # прибавляем единичку к счетчику
+        return self.af  # возвращаем значение суммы
 
 
 if __name__ == '__main__':
-    q = iter(Factorial())
-    print(type(q))
-    for i in range(10):
-        print(next(q))
+    q = iter(Factorial())  # создаем итератор класса
+    print(type(q))  # проверяем тип элемента
+    for i in range(10):  # перебираем первые 10 итераций "ГЕНЕРАТОРА"
+        print(next(q))  # выводим значения по порядку
